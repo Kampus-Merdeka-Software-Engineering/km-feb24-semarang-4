f3d68db059f27a8b65db94050007400bf919d9d3
fikrydestryanto <148188413+fikrydestryanto@users.noreply.github.com>
2024-05-13 19:58:08 +0700

    Penambahan file table-team.html dan about-dataset.html
    - Perubahan file index2.html
    - Penambahan fitur search pada tabel-team.html
    - Penambahan Shadow pada card
    - Header sudah sejajar dengan container

---
+++ main.js
@@ -0,0 +1,10 @@
+// const searchInput = document.getElementById("searchInput")
+// const table = document.getElementById("table")
+// const tableItems = table.getElementsByTagName('th')
+
+// searchInput = addEventListener("keyup", function(e){
+//     const tableTerm = e.target.value.toLowercase()
+//     for( let i = 0; i = tableItems.length; i++){
+//         const text = tableItems[i].textContent.toLocaleLowerCase() 
+//     }
+// })

713ee7cec07842ac3db869c86a14b0cf2165bc28
Muhammad Fadli Isyarqi <Muhammad Fadli Isyarqi>
2024-06-04 22:59:42 +0700

    data table

---
+++ assets/js/list.js
@@ -0,0 +1,36 @@
+// Menunggu sampai seluruh konten DOM dimuat
+document.addEventListener("DOMContentLoaded", () => {
+  // Mengambil data dari file JSON
+  fetch("./datavending.json")
+    .then((res) => res.json()) // Mengubah respons menjadi format JSON
+    .then((data) => {
+      console.log(data);
+      // DataTables
+      let tableBody = document.getElementById("product-table-body"); // Mendapatkan elemen tbody dari tabel produk
+
+      // Mengisi tabel dengan data dari file JSON
+      data.forEach(function (item) {
+        let row = document.createElement("tr"); // Membuat elemen baris baru
+
+        // Menambahkan data produk ke dalam baris
+        row.innerHTML = `
+        <td>${item.Status}</td>
+        <td>${item.Device_ID}</td>
+        <td>${item.Location}</td>
+        <td>${item.Machine}</td>
+        <td>${item.Product}</td>
+        <td>${item.Category}</td>
+        <td>${item.Transaction}</td>
+        <td>${item.TransDate}</td>
+        <td>${item.Type}</td>
+        <td>${item.Prcd_Date}</td>
+        `;
+
+        // Menambahkan baris ke dalam tabel
+        tableBody.appendChild(row);
+      });
+
+      // Menginisialisasi DataTables pada tabel produk
+      $("#product-table").DataTable();
+    });
+});

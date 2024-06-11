02662dde80214613bb8560230ee56db159e400ba
calx10 <allifrizki02@gmail.com>
2024-06-11 08:38:21 +0700

    warna pada line agar tidak random lagi

--- assets/js/chart4.js
+++ assets/js/chart4.js
@@ -29,17 +29,17 @@ const dataByLocation = locations.map((location) => {
       (sale) => ({ x: sale.Bulan, y: parseFloat(sale.Total_Sales) })
     ),
     fill: false,
-    borderColor: getRandomColor(), // Fungsi untuk mendapatkan warna acak
+    borderColor: getRandomBrightColor(), // Fungsi untuk mendapatkan warna terang acak
     tension: 0.1,
   };
 });
 
-// Fungsi untuk mendapatkan warna acak
-function getRandomColor() {
-  const letters = "0123456789ABCDEF";
+// Fungsi untuk mendapatkan warna terang acak
+function getRandomBrightColor() {
+  const letters = "89ABCDEF"; // Menggunakan bagian akhir dari hex untuk warna terang
   let color = "#";
   for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * letters.length)];
   }
   return color;
 }
@@ -48,18 +48,6 @@ const ctx = document.getElementById("linechart").getContext("2d");
 const lineChart = new Chart(ctx, {
   type: "line",
   data: {
-    backgroundColor: [
-      "rgba(255, 99, 132, 0.2)",
-      "rgba(255, 159, 64, 0.2)",
-      "rgba(255, 205, 86, 0.2)",
-      "rgba(75, 192, 192, 0.2)",
-    ],
-    borderColor: [
-      "rgb(255, 99, 132)",
-      "rgb(255, 159, 64)",
-      "rgb(255, 205, 86)",
-      "rgb(75, 192, 192)",
-    ],
     datasets: dataByLocation,
   },
   options: {

85cc530819f2221433ac12176f0bc13f93ce68db
gitaiftah18 <160220473+gitaiftah18@users.noreply.github.com>
2024-06-23 08:03:27 +0700

    Update chart2.js

--- assets/js/chart2.js
+++ assets/js/chart2.js
@@ -1,24 +1,40 @@
-const PercentageSalesByCategory = [{
-  "Category": "Food",
-  "Total_Sales": "9376.75"
-}, {
-  "Category": "Carbonated",
-  "Total_Sales": "5051.25"
-}, {
-  "Category": "Non Carbonated",
-  "Total_Sales": "2681.5"
-}, {
-  "Category": "Water",
-  "Total_Sales": "1221.25"
-}]
+const PercentageSalesByCategory = [
+  {
+    Category: "Food",
+    Total_Sales: 9376.75,
+  },
+  {
+    Category: "Carbonated",
+    Total_Sales: 5051.25,
+  },
+  {
+    Category: "Non Carbonated",
+    Total_Sales: 2681.5,
+  },
+  {
+    Category: "Water",
+    Total_Sales: 1221.25,
+  },
+];
 
-const PercentageSalesSixMonth = [{
-  "type": "Cash",
-  "Total_Sales": "5121.5"
-}, {
-  "type": "Credit",
-  "Total_Sales": "2811.5"
-}]
+const PercentageSalesSixMonth = [
+  {
+    Category: "Food",
+    Total_Sales: 3956.75,
+  },
+  {
+    Category: "Carbonated",
+    Total_Sales: 2223.25,
+  },
+  {
+    Category: "Non Carbonated",
+    Total_Sales: 1125.75,
+  },
+  {
+    Category: "Water",
+    Total_Sales: 627.25,
+  },
+];
 
 const allTimeData6 = PercentageSalesByCategory.map((item) => ({
   Category: item.Category,
@@ -37,12 +53,9 @@ let currentData6 = allTimeCurrentData6;
 
 const ctx = document.getElementById("piechart").getContext("2d");
 
-// Fungsi untuk mendapatkan warna biru secara berurutan
 function getBlueShades() {
   const blueShades = ["#050C9C", "#3572EF", "#3ABEF9", "#A7E6FF"];
-
   let currentIndex = 0;
-
   return function () {
     if (currentIndex >= blueShades.length) {
       currentIndex = 0;
@@ -51,10 +64,7 @@ function getBlueShades() {
   };
 }
 
-// Inisialisasi fungsi untuk mendapatkan warna berikutnya
 const getNextBlueShade = getBlueShades();
-
-// Membuat array warna untuk setiap mesin
 const backgroundColors = allTimeData6.map(() => getNextBlueShade());
 
 let pieChart = new Chart(ctx, {
@@ -82,26 +92,23 @@ let pieChart = new Chart(ctx, {
   },
 });
 
-// Fungsi untuk memperbarui chart dengan data yang diberikan
 function updateChart6(data) {
   pieChart.data.labels = data.map((item) => item.Category);
   pieChart.data.datasets[0].data = data.map((item) => item.Total_Sales);
   pieChart.update();
 }
 
-// Panggil fungsi untuk memperbarui chart saat halaman dimuat
 updateChart6(allTimeCurrentData6);
 
-// Event listener untuk dropdown
 document
   .getElementById("categoryFilter")
   .addEventListener("change", (event) => {
     const selectedCategory = event.target.value;
     if (selectedCategory === "alltime") {
-      currentData6 = allTimeCurrentData6; // Set data yang sedang ditampilkan menjadi data all time
-      updateChart6(allTimeCurrentData6); // Memuat lima produk teratas dari data semua waktu
+      currentData6 = allTimeCurrentData6;
+      updateChart6(allTimeCurrentData6);
     } else if (selectedCategory === "sixmonth") {
-      currentData6 = sixMonthCurrentData6; // Set data yang sedang ditampilkan menjadi data enam bulan
-      updateChart6(sixMonthCurrentData6); // Memuat lima produk teratas dari data enam bulan
+      currentData6 = sixMonthCurrentData6;
+      updateChart6(sixMonthCurrentData6);
     }
   });
